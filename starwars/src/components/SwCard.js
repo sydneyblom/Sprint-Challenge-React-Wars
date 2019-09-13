d8598067d51937e66b0009dcde09947ef8a09541
Sydney Blom <sydneyblom@#gmail.com>
2019-09-13 13:35:45 -0500

    start of changes

--- starwars/src/components/SwCard.js
+++ starwars/src/components/SwCard.js
@@ -2,16 +2,33 @@ import React from "react";
 import styled from "styled-components";
 
 
+const Pulse = styled.div`
+  transition: transform 0.2s ease-in;
+  background: #E0FFFF;
+  color: black;
+  width: 200px;
+  max-height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 20px;
+  cursor: pointer;
+  box-shadow: 0px 1px 6px -2px grey;
 
+  &:hover {
+    transform: translateY(-5px) scale(1.05);
+  }
+`;
 const SwCard = props => {
     return (
-      <div className="char-list" key={props.id}>
+      <Pulse className="char-list" key={props.id}>
         <h2> {props.name}</h2>
         <p>Gender: {props.gender}</p>
         <p>Mass: {props.mass}</p>
         <p>BirthYear:{props.birth_year}</p>
 
-      </div>
+      </Pulse>
     );
   };
   
